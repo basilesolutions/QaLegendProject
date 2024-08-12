71378c7ebcc53210fb8c53c33f251e70f173fd14
Basil <Basil@AIL044>
2024-08-12 18:10:01 +0530

    done

--- selenium/src/main/java/seleniumBasics/Commands.java
+++ selenium/src/main/java/seleniumBasics/Commands.java
@@ -1,12 +1,18 @@
 package seleniumBasics;
 
+import java.awt.AWTException;
+import java.awt.Robot;
+import java.awt.event.KeyEvent;
+import java.security.PublicKey;
 import java.util.List;
+import java.util.Set;
 
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.Select;
 
 public class Commands {
@@ -126,7 +132,151 @@ public class Commands {
 		alert.dismiss();
 		//driver.close();
 	}
-	public static void main(String[] args) {
+	
+	public void verifyRightClick() {
+		
+		WebDriver driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://demo.guru99.com/test/simple_context_menu.html");
+		WebElement rightclick=driver.findElement(By.xpath("//span[@class='context-menu-one btn btn-neutral']"));
+		Actions action =new Actions(driver);
+		action.contextClick(rightclick).build().perform();
+		//driver.close();
+		
+	}
+	 public void verifyDoubleClick() {
+		 WebDriver driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://demo.guru99.com/test/simple_context_menu.html");
+		WebElement doubleclickfield=driver.findElement(By.xpath("//button[text()='Double-Click Me To See Alert']"));
+		Actions actiondouble=new Actions(driver);
+		actiondouble.doubleClick(doubleclickfield).build().perform();
+		Alert alertsimple=driver.switchTo().alert();
+		alertsimple.accept();
+	}
+	 
+	 public void verifymouseHover() {
+		 WebDriver driver=new ChromeDriver();
+			driver.manage().window().maximize();
+			driver.get("https://demoqa.com/menu/");
+			WebElement mainlistmenu=driver.findElement(By.xpath("//a[text()='Main Item 2']"));
+			WebElement submenu=driver.findElement(By.xpath("//a[text()='SUB SUB LIST »']"));
+			Actions action=new Actions(driver);
+			action.moveToElement(mainlistmenu).moveToElement(submenu).build().perform();
+			
+		 	
+	}
+	 public void verifyDragandDrop() {
+		
+		 WebDriver driver=new ChromeDriver();
+		 driver.manage().window().maximize();
+		 driver.get("https://selenium.qabible.in/drag-drop.php");
+		 WebElement dragButton= driver.findElement(By.xpath("//span[text()='Draggable n°1']"));
+		 WebElement dropSection=driver.findElement(By.xpath("//div[@id='mydropzone']"));
+		 Actions act = new Actions(driver);
+		 act.moveToElement(dragButton).build().perform();
+		 act.contextClick(dragButton).build().perform();
+		 act.dragAndDrop(dragButton, dropSection).build().perform();
+		 
+	}
+	 
+	 public void verifyDragandDropUsingOffset() {
+		 WebDriver driver=new ChromeDriver();
+		 driver.manage().window().maximize();
+		 driver.get("https://demoqa.com/dragabble");
+		 WebElement dragButton=driver.findElement(By.xpath("//div[@id='dragBox']"));
+		 Actions action= new Actions(driver);
+		 action.dragAndDropBy(dragButton, 150, 100).build().perform();
+		 
+	}
+	  public void frames() {
+		  
+		     WebDriver driver=new ChromeDriver();
+			 driver.manage().window().maximize();
+			 driver.get("https://demo.guru99.com/test/guru99home/");
+			 WebElement iframe=driver.findElement(By.xpath("//iframe[@id='a077aa5e']"));
+			 driver.switchTo().frame(iframe);
+			 WebElement jmeterImage=driver.findElement(By.xpath("//img[@src=\"Jmeter720.png\"]"));
+			 jmeterImage.click();
+	}
+	  public void multipleWindowHandling() {
+		  WebDriver driver=new ChromeDriver();
+			 driver.manage().window().maximize();
+			 driver.get("https://webdriveruniversity.com/");
+			 WebElement contactus = driver.findElement(By.xpath("//h1[text()='CONTACT US']"));
+			 contactus.click();
+			 WebElement loginbutton = driver.findElement(By.xpath("//h1[text()='LOGIN PORTAL']"));
+			 loginbutton.click();
+			 String parent =driver.getWindowHandle();
+			 System.out.println("parent window"+parent);
+			 Set<String> allWindows=driver.getWindowHandles();
+			 for(String temp:allWindows) {
+				 if(!temp.equals(parent)) {
+				 System.out.println("indows:"+temp);
+				 driver.switchTo().window(temp);
+				 System.out.println(driver.getTitle());
+				 System.out.println(driver.getCurrentUrl());
+				 System.out.println("************");
+			 }
+			 }
+		
+	}
+	  
+	  public void verifyDynamicTable() {
+		  WebDriver driver=new ChromeDriver();
+		  driver.manage().window().maximize();
+		  driver.get("https://money.rediff.com/indices/nse");
+		  WebElement showMoreLink= driver.findElement(By.xpath("//a[@id='showMoreLess']"));
+		  showMoreLink.click();
+		  WebElement table= driver.findElement(By.xpath("//table[@id='dataTable']"));
+		  //System.out.println(table.getText());
+		  WebElement row = driver.findElement(By.xpath("//table[@id='dataTable']/tbody/tr[5]"));
+		  System.out.println(row.getText());
+		  List<WebElement> rows =table.findElements(By.tagName("tr"));
+		  int rowSize =rows.size();
+		  for (int i=0;i<rowSize;i++) {
+			  List<WebElement> columns =rows.get(i).findElements(By.tagName("td"));
+			  int columnSize = columns.size();
+			  for(int j=0;j<columnSize;j++) {
+				  String cellData=columns.get(j).getText();
+				  if(cellData.equals("NIFTY BANK")) {
+					  System.out.println("Previous close value is "+columns.get(1).getText());
+				  }
+			  }
+		  }
+		  driver.close();
+		  
+		 
+	}
+	  
+	  public void verifyFileupload() {
+		  
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://demo.guru99.com/test/upload/");
+		WebElement uploader=driver.findElement(By.xpath("//input[@id='uploadfile_0']"));
+		uploader.sendKeys("C:\\Users\\Basil\\git\\Selenium_Learning\\selenium\\src\\main\\resources\\File upload\\Daily status.txt");
+		WebElement checkbox=driver.findElement(By.xpath("//input[@id='terms']"));
+		checkbox.click();
+		WebElement submitbutton=driver.findElement(By.xpath("//button[@id='submitbutton']"));
+		submitbutton.click();
+		//driver.close();
+	}
+	  
+	  public void verifyKeyBoardEvents() throws AWTException {
+		  
+		  WebDriver driver=new ChromeDriver();
+		  driver.manage().window().maximize();
+		  driver.get("https://demo.guru99.com/");
+		  Robot robot = new Robot();
+		  robot.keyPress(KeyEvent.VK_CONTROL);
+		  robot.keyPress(KeyEvent.VK_T);
+		  robot.keyRelease(KeyEvent.VK_CONTROL);
+		  robot.keyRelease(KeyEvent.VK_T);
+		
+	}
+	  
+	public static void main(String[] args) throws AWTException {
 		// TODO Auto-generated method stub
 		Commands cmd = new Commands();
 		//cmd.verifySwagLabsLogin();
@@ -136,8 +286,18 @@ public class Commands {
 		
 		//cmd.getTotalDropDownValues();
 		//cmd.verifySimpleAlert();
-		cmd.verifyConfirmAlert();
-		cmd.verifyPromptAlert();
+		//cmd.verifyConfirmAlert();
+		//cmd.verifyPromptAlert();
+		//cmd.verifyRightClick();
+		//cmd.verifyDoubleClick();
+		//cmd.verifymouseHover();
+		//cmd.verifyDragandDrop();
+		//cmd.frames();
+		//cmd.multipleWindowHandling();
+		//cmd.verifyDynamicTable();
+		//cmd.verifyFileupload();
+		//cmd.verifyKeyBoardEvents();
+		cmd.verifyDragandDropUsingOffset();
 
 	}
 
